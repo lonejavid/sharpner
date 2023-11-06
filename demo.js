86b9173ad9c9092e6bd707e9bbe2577f0bb04fd3
lonejavid <lonejavid0739@gmail.com>
2023-11-06 19:00:24 +0530

    this is my assignment3

---
+++ demo.js
@@ -0,0 +1,5 @@
+var header=document.getElementById('main-header');
+header.style.borderBottom='solid 3px #000';
+var item=document.getElementById('add-items');
+item.style.color='green';
+item.style.fontWeight='bold';
