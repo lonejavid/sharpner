b0cb9cad4d98004773c1a0569681b816088d5bf7
lonejavid <lonejavid0739@gmail.com>
2023-11-07 08:48:32 +0530

    getElementByTagName added

--- main.js
+++ main.js
@@ -5,3 +5,15 @@ for(var i=0;i<items.length;i++)
     items[i].style.cssText = 'font-weight: bold; color: red;';
 }
 items[2].style.backgroundColor='green';
+var newItem=document.createElement('li');
+newItem.textContent="new-item-added";
+//newItem.classList.add('new-item-added');
+var list=document.querySelector('.list-group');
+list.append(newItem);
+
+//code for getElementByTagName
+var li=document.getElementsByTagName('li');
+for(var i=0;i<li.length;i++)
+{
+    li[i].style.backgroundColor='green';
+}
