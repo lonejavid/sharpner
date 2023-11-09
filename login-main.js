5b20486a5b66497fcf43308f9ec7a466335cdb88
lonejavid <lonejavid0739@gmail.com>
2023-11-09 11:39:42 +0530

    storing the user-details into an object in localStorage

--- login-main.js
+++ login-main.js
@@ -3,11 +3,21 @@ var emailId=document.getElementById('email');
 var myfoem=document.getElementById('myForm');
 //event listener to store user details in local storage
 myfoem.addEventListener('submit',onsubmit);
+//storing the user details in localstorage as an object
 function onsubmit(e)
 {
     e.preventDefault();
     var userName=user.value
    var email=emailId.value
-   localStorage.setItem('user-name',userName);
-   localStorage.setItem('Email-id',email);
+   //creating the object
+  var myobj={
+    user_name :userName,
+    email_id : email
+  };
+  //converting the user data into readable form
+   let obj_serialized=JSON.stringify(myobj);
+   //console.log(obj_serialized);
+ localStorage.setItem('obj',obj_serialized);
+ //again converting data into an object
+ let obj_deserialzed=JSON.parse(localStorage.getItem("obj"));
 }
