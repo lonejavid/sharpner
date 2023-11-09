53daa86a15a3f2b1a628edcf5a6c0ba04fdb8857
lonejavid <lonejavid0739@gmail.com>
2023-11-09 13:29:04 +0530

    displaying the user details and clearing the data once the page is refreshed

--- login-main.js
+++ login-main.js
@@ -7,17 +7,26 @@ myfoem.addEventListener('submit',onsubmit);
 function onsubmit(e)
 {
     e.preventDefault();
-    var userName=user.value
-   var email=emailId.value
+    var userName=user.value;
+   var email=emailId.value;
+   var phone=number.value;
+   var users=document.getElementById('item-list');
+var full_details=userName+"    "+email+"     "+phone;
+
+       var li=document.createElement('li');
+           li.id="person";
+       var new_person=document.createTextNode(full_details);
+       users.append(new_person)
    //creating the object
-  var myobj={
-    user_name :userName,
-    email_id : email
-  };
-  //converting the user data into readable form
-   let obj_serialized=JSON.stringify(myobj);
-   //console.log(obj_serialized);
- localStorage.setItem('obj',obj_serialized);
- //again converting data into an object
- let obj_deserialzed=JSON.parse(localStorage.getItem("obj"));
+//   var myobj={
+//     user_name :userName,
+//     email_id : email
+//   };
+//   //converting the user data into readable form
+//    let obj_serialized=JSON.stringify(myobj);
+//    //console.log(obj_serialized);
+//  localStorage.setItem('obj',obj_serialized);
+//  //again converting data into an object
+//  let obj_deserialzed=JSON.parse(localStorage.getItem("obj"));
+//  document.create
 }
